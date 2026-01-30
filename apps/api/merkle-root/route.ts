cce10b596cfac67bb24b5f5cd7e606c58b350736
Sebastien Coponat <talent@coponatrecords.com>
2026-01-30 20:22:43 +0100

    prisma import?

--- apps/api/merkle-root/route.ts
+++ apps/api/merkle-root/route.ts
@@ -1,4 +1,4 @@
-import  prisma  from '@repo/database';
+import  {prisma}  from '@repo/database';
 
 export async function GET() {
   try {
--- apps/web/backend/api/merkle-root/route.ts
+++ apps/web/backend/api/merkle-root/route.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@prisma/prisma';
+import  {prisma}  from '@repo/database';
 
 export async function GET() {
   try {
